ede68485cfcbad74ae7428d75c9b04644123d124
goncalonunes22005787 <79208557+goncalonunes22005787@users.noreply.github.com>
2022-04-14 15:39:56 +0100

    Add files via upload

---
+++ app/src/main/java/pt/ulusofona/deisi/cm2122/g22005787_22005793/MainActivity.kt
@@ -0,0 +1,11 @@
+package pt.ulusofona.deisi.cm2122.g22005787_22005793
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
