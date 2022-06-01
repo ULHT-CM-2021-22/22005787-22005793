78c26080d8b9b9a8d9fb3e16af4100283ad4eff4
goncalonunes22005787 <a22005787@alunos.ulht.pt>
2022-06-01 13:19:20 +0100

    Ligação a API implementada (2)

---
+++ app/build/tmp/kapt3/stubs/debug/pt/ulusofona/deisi/cm2122/g22005787_22005793/Fires.java
@@ -0,0 +1,168 @@
+package pt.ulusofona.deisi.cm2122.g22005787_22005793;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b\u001e\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001BU\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0003\u0012\u0006\u0010\u0007\u001a\u00020\b\u0012\u0006\u0010\t\u001a\u00020\b\u0012\u0006\u0010\n\u001a\u00020\b\u0012\u0006\u0010\u000b\u001a\u00020\u0003\u0012\u0006\u0010\f\u001a\u00020\u0003\u0012\u0006\u0010\r\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u000eJ\t\u0010\u001b\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001c\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001d\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001e\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0003H\u00c6\u0003J\t\u0010 \u001a\u00020\bH\u00c6\u0003J\t\u0010!\u001a\u00020\bH\u00c6\u0003J\t\u0010\"\u001a\u00020\bH\u00c6\u0003J\t\u0010#\u001a\u00020\u0003H\u00c6\u0003J\t\u0010$\u001a\u00020\u0003H\u00c6\u0003Jm\u0010%\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00032\b\b\u0002\u0010\u0006\u001a\u00020\u00032\b\b\u0002\u0010\u0007\u001a\u00020\b2\b\b\u0002\u0010\t\u001a\u00020\b2\b\b\u0002\u0010\n\u001a\u00020\b2\b\b\u0002\u0010\u000b\u001a\u00020\u00032\b\b\u0002\u0010\f\u001a\u00020\u00032\b\b\u0002\u0010\r\u001a\u00020\u0003H\u00c6\u0001J\u0013\u0010&\u001a\u00020\'2\b\u0010(\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010)\u001a\u00020\bH\u00d6\u0001J\t\u0010*\u001a\u00020\u0003H\u00d6\u0001R\u0016\u0010\n\u001a\u00020\b8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u0010R\u0016\u0010\u0005\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0011\u0010\u0012R\u0016\u0010\f\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u0012R\u0016\u0010\u0004\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0012R\u0016\u0010\r\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0012R\u0016\u0010\u0006\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\u0012R\u0016\u0010\u0002\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\u0012R\u0016\u0010\u0007\u001a\u00020\b8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0010R\u0016\u0010\u000b\u001a\u00020\u00038\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u0012R\u0016\u0010\t\u001a\u00020\b8\u0006X\u0087\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001a\u0010\u0010\u00a8\u0006+"}, d2 = {"Lpt/ulusofona/deisi/cm2122/g22005787_22005793/Fires;", "", "id", "", "district", "concelho", "freguesia", "man", "", "terrain", "aerial", "status", "data", "extra", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;IIILjava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getAerial", "()I", "getConcelho", "()Ljava/lang/String;", "getData", "getDistrict", "getExtra", "getFreguesia", "getId", "getMan", "getStatus", "getTerrain", "component1", "component10", "component2", "component3", "component4", "component5", "component6", "component7", "component8", "component9", "copy", "equals", "", "other", "hashCode", "toString", "app_debug"})
+public final class Fires {
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "id")
+    private final java.lang.String id = null;
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "district")
+    private final java.lang.String district = null;
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "concelho")
+    private final java.lang.String concelho = null;
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "freguesia")
+    private final java.lang.String freguesia = null;
+    @com.google.gson.annotations.SerializedName(value = "man")
+    private final int man = 0;
+    @com.google.gson.annotations.SerializedName(value = "terrain")
+    private final int terrain = 0;
+    @com.google.gson.annotations.SerializedName(value = "aerial")
+    private final int aerial = 0;
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "status")
+    private final java.lang.String status = null;
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "data")
+    private final java.lang.String data = null;
+    @org.jetbrains.annotations.NotNull()
+    @com.google.gson.annotations.SerializedName(value = "extra")
+    private final java.lang.String extra = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires copy(@org.jetbrains.annotations.NotNull()
+    java.lang.String id, @org.jetbrains.annotations.NotNull()
+    java.lang.String district, @org.jetbrains.annotations.NotNull()
+    java.lang.String concelho, @org.jetbrains.annotations.NotNull()
+    java.lang.String freguesia, int man, int terrain, int aerial, @org.jetbrains.annotations.NotNull()
+    java.lang.String status, @org.jetbrains.annotations.NotNull()
+    java.lang.String data, @org.jetbrains.annotations.NotNull()
+    java.lang.String extra) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public Fires(@org.jetbrains.annotations.NotNull()
+    java.lang.String id, @org.jetbrains.annotations.NotNull()
+    java.lang.String district, @org.jetbrains.annotations.NotNull()
+    java.lang.String concelho, @org.jetbrains.annotations.NotNull()
+    java.lang.String freguesia, int man, int terrain, int aerial, @org.jetbrains.annotations.NotNull()
+    java.lang.String status, @org.jetbrains.annotations.NotNull()
+    java.lang.String data, @org.jetbrains.annotations.NotNull()
+    java.lang.String extra) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getId() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getDistrict() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getConcelho() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getFreguesia() {
+        return null;
+    }
+    
+    public final int component5() {
+        return 0;
+    }
+    
+    public final int getMan() {
+        return 0;
+    }
+    
+    public final int component6() {
+        return 0;
+    }
+    
+    public final int getTerrain() {
+        return 0;
+    }
+    
+    public final int component7() {
+        return 0;
+    }
+    
+    public final int getAerial() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component8() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getStatus() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component9() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getData() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component10() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getExtra() {
+        return null;
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/pt/ulusofona/deisi/cm2122/g22005787_22005793/firesList.java
@@ -0,0 +1,71 @@
+package pt.ulusofona.deisi.cm2122.g22005787_22005793;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u0012\u0012\u0004\u0012\u00020\u00020\u0001j\b\u0012\u0004\u0012\u00020\u0002`\u0003B\u0005\u00a2\u0006\u0002\u0010\u0004\u00a8\u0006\u0005"}, d2 = {"Lpt/ulusofona/deisi/cm2122/g22005787_22005793/firesList;", "Ljava/util/ArrayList;", "Lpt/ulusofona/deisi/cm2122/g22005787_22005793/Fires;", "Lkotlin/collections/ArrayList;", "()V", "app_debug"})
+public final class firesList extends java.util.ArrayList<pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires> {
+    
+    public firesList() {
+        super(0);
+    }
+    
+    @java.lang.Override()
+    public final boolean contains(java.lang.Object element) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public boolean contains(pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires element) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int getSize() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public final int indexOf(java.lang.Object element) {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public int indexOf(pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires element) {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public final int lastIndexOf(java.lang.Object element) {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public int lastIndexOf(pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires element) {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    public final pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires remove(int p0) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public final boolean remove(java.lang.Object element) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public boolean remove(pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires element) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public pt.ulusofona.deisi.cm2122.g22005787_22005793.Fires removeAt(int p0) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public final int size() {
+        return 0;
+    }
+}
