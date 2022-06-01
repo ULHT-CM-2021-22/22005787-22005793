3f82fc26dc83749f7aa3c0a44b276838b53df476
goncalonunes22005787 <a22005787@alunos.ulht.pt>
2022-06-01 15:49:53 +0100

    Implementação do repositorio (2)

---
+++ app/build/tmp/kapt3/stubs/debug/pt/ulusofona/deisi/cm2122/g22005787_22005793/ConnectivityUtil.java
@@ -0,0 +1,19 @@
+package pt.ulusofona.deisi.cm2122.g22005787_22005793;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\tR\u0016\u0010\u0003\u001a\n \u0005*\u0004\u0018\u00010\u00040\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\n"}, d2 = {"Lpt/ulusofona/deisi/cm2122/g22005787_22005793/ConnectivityUtil;", "", "()V", "TAG", "", "kotlin.jvm.PlatformType", "isOnline", "", "context", "Landroid/content/Context;", "app_debug"})
+public final class ConnectivityUtil {
+    @org.jetbrains.annotations.NotNull()
+    public static final pt.ulusofona.deisi.cm2122.g22005787_22005793.ConnectivityUtil INSTANCE = null;
+    private static final java.lang.String TAG = null;
+    
+    private ConnectivityUtil() {
+        super();
+    }
+    
+    public final boolean isOnline(@org.jetbrains.annotations.NotNull()
+    android.content.Context context) {
+        return false;
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/pt/ulusofona/deisi/cm2122/g22005787_22005793/FireApplication.java
@@ -0,0 +1,15 @@
+package pt.ulusofona.deisi.cm2122.g22005787_22005793;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H\u0016\u00a8\u0006\u0005"}, d2 = {"Lpt/ulusofona/deisi/cm2122/g22005787_22005793/FireApplication;", "Landroid/app/Application;", "()V", "onCreate", "", "app_debug"})
+public final class FireApplication extends android.app.Application {
+    
+    public FireApplication() {
+        super();
+    }
+    
+    @java.lang.Override()
+    public void onCreate() {
+    }
+}
