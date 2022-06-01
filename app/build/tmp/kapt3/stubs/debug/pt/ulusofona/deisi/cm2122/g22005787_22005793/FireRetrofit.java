a5a6315adfa66e7516fc4a6d1576b5890918aa39
goncalonunes22005787 <a22005787@alunos.ulht.pt>
2022-06-01 12:43:57 +0100

    Ligação a API por fazer (2)

---
+++ app/build/tmp/kapt3/stubs/debug/pt/ulusofona/deisi/cm2122/g22005787_22005793/FireRetrofit.java
@@ -0,0 +1,47 @@
+package pt.ulusofona.deisi.cm2122.g22005787_22005793;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000F\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u001e\u0010\n\u001a\u00020\u000b2\f\u0010\f\u001a\b\u0012\u0004\u0012\u00020\u000b0\r2\u0006\u0010\u000e\u001a\u00020\u000fH\u0016J\u001c\u0010\u0010\u001a\u00020\u00062\u0012\u0010\f\u001a\u000e\u0012\u0004\u0012\u00020\u0006\u0012\u0004\u0012\u00020\u000b0\u0011H\u0016J$\u0010\u0010\u001a\u00020\u00062\u0012\u0010\f\u001a\u000e\u0012\u0004\u0012\u00020\u0006\u0012\u0004\u0012\u00020\u000b0\u00112\u0006\u0010\u0012\u001a\u00020\u0006H\u0016J\"\u0010\u0013\u001a\u00020\u000b2\u0018\u0010\f\u001a\u0014\u0012\n\u0012\b\u0012\u0004\u0012\u00020\u00150\u0014\u0012\u0004\u0012\u00020\u000b0\u0011H\u0016J\u001c\u0010\u0016\u001a\u00020\u00062\u0012\u0010\f\u001a\u000e\u0012\u0004\u0012\u00020\u0006\u0012\u0004\u0012\u00020\u000b0\u0011H\u0016R\u0016\u0010\u0005\u001a\n \u0007*\u0004\u0018\u00010\u00060\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0016\u0010\b\u001a\n \u0007*\u0004\u0018\u00010\t0\tX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0017"}, d2 = {"Lpt/ulusofona/deisi/cm2122/g22005787_22005793/FireRetrofit;", "Lpt/ulusofona/deisi/cm2122/g22005787_22005793/FireModel;", "retrofit", "Lretrofit2/Retrofit;", "(Lretrofit2/Retrofit;)V", "TAG", "", "kotlin.jvm.PlatformType", "service", "Lpt/ulusofona/deisi/cm2122/g22005787_22005793/FiresService;", "addToHistory", "", "onFinished", "Lkotlin/Function0;", "fireData", "Lpt/ulusofona/deisi/cm2122/g22005787_22005793/FireRoom;", "fogosNaRegiao", "Lkotlin/Function1;", "regiao", "getHistory", "", "Lpt/ulusofona/deisi/cm2122/g22005787_22005793/FireData;", "totalFogos", "app_debug"})
+public final class FireRetrofit extends pt.ulusofona.deisi.cm2122.g22005787_22005793.FireModel {
+    private final java.lang.String TAG = null;
+    private final pt.ulusofona.deisi.cm2122.g22005787_22005793.FiresService service = null;
+    
+    public FireRetrofit(@org.jetbrains.annotations.NotNull()
+    retrofit2.Retrofit retrofit) {
+        super();
+    }
+    
+    @java.lang.Override()
+    public void getHistory(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super java.util.List<pt.ulusofona.deisi.cm2122.g22005787_22005793.FireData>, kotlin.Unit> onFinished) {
+    }
+    
+    @java.lang.Override()
+    public void addToHistory(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function0<kotlin.Unit> onFinished, @org.jetbrains.annotations.NotNull()
+    pt.ulusofona.deisi.cm2122.g22005787_22005793.FireRoom fireData) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String fogosNaRegiao(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super java.lang.String, kotlin.Unit> onFinished) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String fogosNaRegiao(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super java.lang.String, kotlin.Unit> onFinished, @org.jetbrains.annotations.NotNull()
+    java.lang.String regiao) {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String totalFogos(@org.jetbrains.annotations.NotNull()
+    kotlin.jvm.functions.Function1<? super java.lang.String, kotlin.Unit> onFinished) {
+        return null;
+    }
+}
